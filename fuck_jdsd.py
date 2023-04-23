22566d7e3640943e13062bcc0bbe80a9f3efc174
el2k <107855575+el2k@users.noreply.github.com>
2023-04-23 16:25:01 +0800

    Update fuck_jdsd.py

--- fuck_jdsd.py
+++ fuck_jdsd.py
@@ -8,7 +8,7 @@ from loguru import logger
 '''
 下方填写key 需抓包  key在更换微信登录后会改变 具体有效期尚未可知
 '''
-key = 'iJjhlQE29BSQN381NHjbJyXLWSvczjkTwcniqVHKreGl/LbQjWzHAgigncjMS4K/'
+key = 'ueK6IeaiEniPfPmvhwTJgaa+rh2Nhjx/20vR1HAVrFpqXdpeFPHhgwO8t4OQLDj9'
 session = requests.session()
 headers = {
   'Host': 'jdsd.gzhu.edu.cn',
